b17b826fce6337a11e735ac2d8743df9bf9c40e2
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2025-05-29 17:33:15 +0300

    boilerplate code for pages

--- defs.h
+++ defs.h
@@ -1,24 +1,24 @@
 #pragma once
 
-#define ALARM_MIN(x)   ((x >> 0) & 0xFF)
-#define ALARM_HOUR(x)  ((x >> 8) & 0xFF)
-#define ALARM_DAYS(x)  ((x >> 16) & 0x7F)
-#define ALARM_ON(x)    ((x >> 23) & 0x01)
+#define ALARM_MIN(x)                ((x >> 0) & 0xFF)
+#define ALARM_HOUR(x)               ((x >> 8) & 0xFF)
+#define ALARM_DAYS(x)               ((x >> 16) & 0x7F)
+#define ALARM_ON(x)                 ((x >> 23) & 0x01)
 
-#define MODE_NORMAL         0
-#define MODE_CONTROL        1
+#define MODE_NORMAL                 0
+#define MODE_CONTROL                1
 
-#define MODE_NORMAL_TITLE   "NORMAL"
-#define MODE_CONTROL_TITLE  "CONTROL"
+#define MODE_NORMAL_TITLE           "NORMAL"
+#define MODE_CONTROL_TITLE          "CONTROL"
 
-#define PAGE_HOME           0
-#define PAGE_MUSIC          1
-#define PAGE_ALARMS         2
-#define PAGE_NOTIFS         3
+#define PAGE_HOME                   0
+#define PAGE_MUSIC                  1
+#define PAGE_ALARMS                 2
+#define PAGE_NOTIFICATIONS          3
 
-#define PAGE_MAX            PAGE_NOTIFS
+#define PAGE_MAX                    PAGE_NOTIFICATIONS
 
-#define PAGE_HOME_TITLE     "HOME"
-#define PAGE_MUSIC_TITLE    "MUSIC"
-#define PAGE_ALARMS_TITLE   "ALARMS"
-#define PAGE_NOTIFS_TITLE   "NOTIFS"
+#define PAGE_HOME_TITLE             "HOME"
+#define PAGE_MUSIC_TITLE            "MUSIC"
+#define PAGE_ALARMS_TITLE           "ALARMS"
+#define PAGE_NOTIFICATIONS_TITLE    "NOTIFY"
